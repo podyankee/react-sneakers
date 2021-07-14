2e8a856ee5843d1da9760004483164c318c0173f
podyankee <podyankee@gmail.com>
2021-07-14 03:19:22 +0300

    card placeholders during rendering

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import ContentLoader from 'react-content-loader';
 import styles from './Card.module.sass';
 
 function Card({
@@ -10,6 +11,7 @@ function Card({
 	onPlus,
 	favorited = false,
 	added = false,
+	loading = false,
 }) {
 	const [isAdded, setIsAdded] = useState(added);
 	const [isFavorite, setIsFavorite] = useState(favorited);
@@ -26,23 +28,44 @@ function Card({
 
 	return (
 		<div className={styles.card}>
-			<div className={styles.favorite} onClick={onClickFavorite}>
-				<img src={isFavorite ? '/img/heart-liked.svg' : '/img/heart-unliked.svg'} alt="Unliked" />
-			</div>
-			<img width={133} height={112} src={imageUrl} alt={title} />
-			<h5>{title}</h5>
-			<div className="d-flex justify-between align-center">
-				<div className="d-flex flex-column">
-					<span>Цена:</span>
-					<b>{price} грн.</b>
-				</div>
-				<img
-					className={styles.plus}
-					src={isAdded ? '/img/btn-checked.svg' : '/img/btn-plus.svg'}
-					alt="Plus"
-					onClick={onClickPlus}
-				/>
-			</div>
+			{loading ? (
+				<ContentLoader
+					backgroundColor="#f3f3f3"
+					speed={2}
+					width={155}
+					height={265}
+					viewBox="0 0 160 265"
+					foregroundColor="#ecebeb">
+					<rect x="0" y="0" rx="10" ry="10" width="160" height="155" />
+					<rect x="0" y="167" rx="5" ry="5" width="160" height="15" />
+					<rect x="0" y="187" rx="5" ry="5" width="100" height="15" />
+					<rect x="0" y="234" rx="5" ry="5" width="80" height="25" />
+					<rect x="124" y="230" rx="10" ry="10" width="32" height="32" />
+				</ContentLoader>
+			) : (
+				<>
+					<div className={styles.favorite} onClick={onClickFavorite}>
+						<img
+							src={isFavorite ? '/img/heart-liked.svg' : '/img/heart-unliked.svg'}
+							alt="Unliked"
+						/>
+					</div>
+					<img width="100%" height={135} src={imageUrl} alt={title} />
+					<h5>{title}</h5>
+					<div className="d-flex justify-between align-center">
+						<div className="d-flex flex-column">
+							<span>Цена:</span>
+							<b>{price} грн.</b>
+						</div>
+						<img
+							className={styles.plus}
+							src={isAdded ? '/img/btn-checked.svg' : '/img/btn-plus.svg'}
+							alt="Plus"
+							onClick={onClickPlus}
+						/>
+					</div>
+				</>
+			)}
 		</div>
 	);
 }
